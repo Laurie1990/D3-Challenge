1dd7f4bbdf0205ddbc3eb9f0b137f61849bc197d
Laurie1990 <63030449+Laurie1990@users.noreply.github.com>
2020-10-27 01:39:19 +1100

    further changes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,7 +6,7 @@ d3.csv("assets/data/data.csv").then(function(Health) {
         d.healthcare = +d.healthcare;
         d.abbr = d.abbr;
     }); 
-
+//--------------------------------------------------------------------------
 
 //2. Set-out layout
 var svgWidth = 1000;
@@ -31,7 +31,7 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
+//-----------------------------------------------------------------------------------
     // 3. Create axes
     xlinearScale = d3.scaleLinear().domain([d3.min(Health,d=>d.poverty)-0.5, d3.max(Health,d=>d.poverty)+1]).range([0,chartwidth]);
     ylinearScale = d3.scaleLinear().domain([d3.min(Health,d=>d.healthcare)-2, d3.max(Health,d=>d.healthcare)+1]).range([chartheight,0]);
@@ -39,11 +39,12 @@ var chartGroup = svg.append("g")
     var xAxis = d3.axisBottom(xlinearScale);
     var yAxis   = d3.axisLeft(ylinearScale);
 
-    // 4. Append axis to chartGroup.
     chartGroup.append("g").attr("transform",`translate(0, ${chartheight})`).call(xAxis);
     chartGroup.append("g").call(yAxis);
 
-    // 5. Add circles and text
+//-----------------------------------------------------------------------------------------
+
+    // 4. Add circles and text
     chartGroup.selectAll("circle")
               .data(Health)
               .enter()
@@ -64,8 +65,9 @@ var chartGroup = svg.append("g")
               .style("fill","white")
               .style("font-size",10)
               .style("font-weight","bold");
+//--------------------------------------------------------------------------------------
     
-    // 6. Ad axes lables
+    // 5. Ad axes lables
     chartGroup.append("text")
     .attr("transform", `translate(${chartwidth / 2}, ${chartheight + margin.top + 25})`)
     .attr("class", "axisText")
@@ -82,5 +84,5 @@ var chartGroup = svg.append("g")
     .text("Without health insurance (%)")
     .style("font-size", "20px")
     .style("font-weight","bold");
-
+//----------------------------------------------------------------------------------------
 });   
