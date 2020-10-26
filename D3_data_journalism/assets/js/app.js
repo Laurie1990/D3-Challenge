dc2e4a424dfa94f4fd13cb60442fbbee25047fc7
Laurie1990 <63030449+Laurie1990@users.noreply.github.com>
2020-10-26 22:07:14 +1100

    created graph

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,7 +6,7 @@ d3.csv("assets/data/data.csv").then(function(GetData) {
    GetData.forEach(x => {
         x.poverty = +x.poverty;
         x.healthcare = +x.healthcare;
-    });
+    })});
 
 
 //Set-up page
@@ -20,7 +20,7 @@ var margin = {
     left: 50
             };
 
-// Set chart width and height
+// Set visualisation width and height
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -30,5 +30,9 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-  var chartGroup = svg.append("g")
+//Create graphs
+var Charts = svg.append("c")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  xtick = d3.scaleLinear().domain([d3.min(GetData,x=>x.poverty)-0.5, d3.max(GetData,x=>x.poverty)+1]).range([0,chartwidth]);
+  ytick = d3.scaleLinear().domain([d3.min(GetData,x=>x.healthcare)-2, d3.max(GetData,x=>x.healthcare)+1]).range([chartheight,0]);
