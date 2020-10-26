140a9b19a0f83d99b19b1e8ee096d383874d221c
Laurie1990 <63030449+Laurie1990@users.noreply.github.com>
2020-10-26 21:31:38 +1100

    set up chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,15 @@
 // @TODO: YOUR CODE HERE!
+//1. Set-up chart
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 30,
+    right: 50,
+    bottom: 60, 
+    left: 50
+            };
+
+// Set chart width and height
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
